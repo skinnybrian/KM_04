df7136bd697f7dbeabbb1fa1fa0d9f3ce8bfce60
maekawakazuma <yana.sd.iml@gmail.com>
2015-11-28 10:56:24 +0900

    .gitignoreでcsv無視しない

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+CSV.foreach('db/seed/seed.csv') do |row|
