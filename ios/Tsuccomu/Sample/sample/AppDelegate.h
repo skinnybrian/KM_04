c77af75fad0301bd1c281e1e8eb75556c7b3d998
okasho <okasho1010@gmail.com>
2015-11-28 10:53:28 +0900

    recreate ios project

---
+++ ios/Tsuccomu/Sample/sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SRClientApp
+//
+//  Created by NTT IT on 2015.
+//  Copyright(C) 2015 NTT IT CORPORATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/Tsuccomu/Sample/sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SRClientApp
+//
+//  Created by 木村朋大 on 2013/10/08.
+//  Copyright (c) 2013年 speechrec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
